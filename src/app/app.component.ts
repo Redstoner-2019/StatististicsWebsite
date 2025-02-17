7277d1803e8403a6f724b4456151a887b7368934
Redstoner <85954818+Redstoner-2019@users.noreply.github.com>
2025-02-17 11:37:52 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,42 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  params: any | null = {};
+  changeThemeButtonText = "Change Theme";
+
+  constructor(private themeService: ThemeService, private route: ActivatedRoute, private router: Router) {
+
+  }
+
+  changeThemeButton(){
+    if(this.router.url.startsWith("/themes")){
+      this.changeThemeButtonText = "Change Theme";
+      console.log("Redirecting to " + this.params.redirect);
+      this.router.navigate([this.params.redirect], { queryParams: JSON.parse(this.params.params) });
+    } else {
+      this.changeThemeButtonText = "Back";
+      this.router.navigate(["/themes"], { queryParams: { redirect: this.router.url, params: JSON.stringify(this.params) } });
+    }
+    console.log(this.router.url);
+  }
+
+  update(){
+    let theme = localStorage.getItem("theme");
+    if(theme == null) theme = "lofi";
+    this.switchTheme(theme);
+  }
+
+  switchTheme(theme:string){
+    this.themeService.setTheme(theme);
+    localStorage.setItem("theme", theme);
+  }
+
   title = 'StatWebsite';
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.params = params;
+      console.log(this.params);
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TwofactorComponent } from './twofactor/twofactor.component';
+import { ThemeSelectorComponent } from './theme-selector/theme-selector.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '2fa', component: TwofactorComponent},
+    {path: 'themes', component: ThemeSelectorComponent},
+    {path: '**', redirectTo: 'dashboard'},
+];
