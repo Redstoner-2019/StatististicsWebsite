5af83d4bf23405327cf91187a70212f5f5e2c678
Redstoner <85954818+Redstoner-2019@users.noreply.github.com>
2025-02-17 23:02:31 +0100

    done nothing

---
+++ src/app/game-object/game-object.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-game-object',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-object.component.html',
+  styleUrl: './game-object.component.scss'
+})
+export class GameObjectComponent {
+  @Input() name!: string;
+  @Input() players!: number;
+  @Input() uuid!: string;
+}
