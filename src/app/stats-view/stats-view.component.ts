f556dd13c141ca0fd89d61dc7d79d568fa3e1ecf
Paepke, Lukas <l.paepke@optadata.de>
2025-04-07 16:22:40 +0200

    fuck yall

--- src/app/stats-view/stats-view.component.ts
+++ src/app/stats-view/stats-view.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpClientModule  } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms'; 
@@ -8,7 +8,7 @@ import { StatEntryComponent } from "../stat-entry/stat-entry.component";
 @Component({
   selector: 'app-stats-view',
   standalone: true,
-  imports: [FormsModule, CommonModule, StatEntryComponent],
+  imports: [FormsModule, CommonModule, StatEntryComponent, HttpClientModule],
   templateUrl: './stats-view.component.html',
   styleUrl: './stats-view.component.scss'
 })
@@ -33,7 +33,23 @@ export class StatsViewComponent {
 
   challengeMapping: any = {};
 
+  page = 0;
+  maxpages = 2;
+  pageSize = 10;
+
   constructor(private http: HttpClient, private route: ActivatedRoute) {
+  }
+
+  pageMinus(){
+    this.page--;
+    if(this.page < 0) this.page = 0;
+    this.updateData();
+  }
+
+  pagePlus(){
+    this.page++;
+    if(this.page >= this.maxpages) this.page = this.maxpages-1;
+    this.updateData();
   }
 
   updateData() {
@@ -50,8 +66,6 @@ export class StatsViewComponent {
       "game": this.game,
       "version": this.version
     };
-
-    console.log(packetAllChallenges);
 
     let apiUrl = 'http://localhost:8082/stats/challenges/getAll';
 
@@ -88,20 +102,16 @@ export class StatsViewComponent {
       this.challengeDisplay = result.name;
     });
 
-    setTimeout(function() {
-
-    },500)
-
     this.challenge = this.challengeMapping[this.challengeDisplay];
 
     let packet = {
-      "pageNumber": 0,
-      "pageSize": 5,
+      "pageNumber": this.page,
+      "pageSize": this.pageSize,
       "zeroIndex": true,
       "ascending": true,
       "sortBy": this.sort,
       "game": this.game,
-      "challenge": this.challenge,
+      "challenge": "8d7a0f25-0f78-41d1-9ac9-ce4bf1237eb9",
       "version": this.version
     };
 
@@ -115,12 +125,13 @@ export class StatsViewComponent {
       let i:number = 0;
 
       this.entries = [];
+      this.maxpages = result.pages;
 
       for(const entry of data){
         i++;
 
         const e: Entry = {
-          place: i.toString(),
+          place: (i + (this.page * this.pageSize)).toString(),
           username: entry.username.toString(),
           value: entry.data[this.sort].toString(),
           date: entry.created.toString()
